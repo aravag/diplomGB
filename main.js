8ad1633ec16a88e5684a23ea488b3a1412474855
AAR89 <ru.booking@yandex.ru>
2024-02-19 21:35:43 +0300

    add fetch

--- main.js
+++ main.js
@@ -252,3 +252,22 @@ function init() {
 }
 
 init();
+
+// async function getResponse(data) {
+//     try {
+//         const response = await fetch("https://example.com/profile", {
+//             method: "POST", // or 'PUT'
+//             headers: {
+//                 "Content-Type": "application/json",
+//             },
+//             body: JSON.stringify(data),
+//         });
+
+//         const result = await response.json();
+//         console.log("Success:", result);
+//     } catch (error) {
+//         console.error("Error:", error);
+//     }
+// }
+
+// getResponse();
